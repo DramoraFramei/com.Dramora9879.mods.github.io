1772b664705dbf589b51359fd5dadc0a32666652
DramoraFramei <dramorajframei@gmail.com>
2026-09-03 12:10:45 -0400

    Add MineAllergies and MineAllergiesClient classes, implement mod setup and configuration screen

--- Mine Allergies/src/main/java/com/Dramora9879/mods/mine_allergies/MineAllergies.java
+++ Mine Allergies/src/main/java/com/Dramora9879/mods/mine_allergies/MineAllergies.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
@@ -20,11 +19,9 @@ import net.minecraft.world.level.material.MapColor;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
@@ -35,12 +32,12 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(Mine_Allergies.MODID)
-public class Mine_Allergies {
+@Mod(MineAllergies.MODID)
+public class MineAllergies {
 	// Define mod id in a commonplace for everything to reference
 	public static final String MODID = "mine_allergies";
 	// Directly reference a slf4j logger
-	private static final Logger LOGGER = LogUtils.getLogger();
+	public static final Logger LOGGER = LogUtils.getLogger();
 	// Create a Deferred Register to hold Blocks which will all be registered under the "mine_allergies" namespace
 	public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
 	// Create a Deferred Register to hold Items which will all be registered under the "mine_allergies" namespace
@@ -54,16 +51,21 @@ public class Mine_Allergies {
 	public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
 
 	// Creates a new food item with the id "mine_allergies:example_id", nutrition 1 and saturation 2
-	public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder().alwaysEdible().nutrition(1).saturationModifier(2f).build()));
+	public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
+			.alwaysEdible().nutrition(1).saturationModifier(2f).build()));
 
 	// Creates a creative tab with the id "mine_allergies:example_tab" for the example item, that is placed after the combat tab
-	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder().title(Component.translatable("itemGroup.mine_allergies")).withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> EXAMPLE_ITEM.get().getDefaultInstance()).displayItems((parameters, output) -> {
-		output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-	}).build());
+	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+			.title(Component.translatable("itemGroup.mine_allergies")) //The language key for the title of your CreativeModeTab
+			.withTabsBefore(CreativeModeTabs.COMBAT)
+			.icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+			.displayItems((parameters, output) -> {
+				output.accept(EXAMPLE_ITEM.get());// Add the example item to the tab. For your own tabs, this method is preferred over the event
+			}).build());
 
 	// The constructor for the mod class is the first code that is run when your mod is loaded.
 	// FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-	public Mine_Allergies(IEventBus modEventBus, ModContainer modContainer) {
+	public MineAllergies(IEventBus modEventBus, ModContainer modContainer) {
 		// Register the commonSetup method for modloading
 		modEventBus.addListener(this::commonSetup);
 
@@ -75,7 +77,7 @@ public class Mine_Allergies {
 		CREATIVE_MODE_TABS.register(modEventBus);
 
 		// Register ourselves for server and other game events we are interested in.
-		// Note that this is necessary if and only if we want *this* class (Mine_allergies) to respond directly to events.
+		// Note that this is necessary if and only if we want *this* class (MineAllergies) to respond directly to events.
 		// Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
 		NeoForge.EVENT_BUS.register(this);
 
@@ -86,20 +88,24 @@ public class Mine_Allergies {
 		modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
 	}
 
-	private void commonSetup(final FMLCommonSetupEvent event) {
+	private void commonSetup(FMLCommonSetupEvent event) {
 		// Some common setup code
 		LOGGER.info("HELLO FROM COMMON SETUP");
 
-		if (Config.logDirtBlock) LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
+		if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
+			LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
+		}
 
-		LOGGER.info("{}{}", Config.magicNumberIntroduction, Config.magicNumber);
+		LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
 
-		Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+		Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
 	}
 
 	// Add the example block item to the building blocks tab
 	private void addCreative(BuildCreativeModeTabContentsEvent event) {
-		if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) event.accept(EXAMPLE_BLOCK_ITEM);
+		if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+			event.accept(EXAMPLE_BLOCK_ITEM);
+		}
 	}
 
 	// You can use SubscribeEvent and let the Event Bus discover methods to call
@@ -107,16 +113,5 @@ public class Mine_Allergies {
 	public void onServerStarting(ServerStartingEvent event) {
 		// Do something when the server starts
 		LOGGER.info("HELLO from server starting");
-	}
-
-	// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-	@EventBusSubscriber(modid = MODID, value = Dist.CLIENT)
-	public static class ClientModEvents {
-		@SubscribeEvent
-		public static void onClientSetup(FMLClientSetupEvent event) {
-			// Some client setup code
-			LOGGER.info("HELLO FROM CLIENT SETUP");
-			LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-		}
 	}
 }
